e48e13338008ff9f3d18c0f75e0d3c7674718020
nat <naesst@gmail.com>
2015-12-26 00:03:26 +0100

    Colores en filas más creación de constantes en PHP
    Código anterior, pero se colorean las filas alternando gris y blanco.
    Además, el tamaño es una constante: define(TAM, 10).

--- tabla.php
+++ tabla.php
@@ -1,11 +1,24 @@
 <?php 
+
+$fila = 1;
+define("TAM", 10);
+define("COLOR_IMPAR", "#FFFFFF");
+define("COLOR_PAR", "#CCCCCC");
+
 echo "<html>";
 echo "<table border='1'>";
-echo "<tr>";
+echo "<tr bgcolor=".COLOR_IMPAR.">";
+
+
 for ($i=1; $i<101; $i++){
     echo "<td>" . $i. "</td>";
-    if ($i % 10 === 0){
-        echo "</tr><tr>";
+    if ($i % TAM === 0){
+        if ($fila % 2 === 0){
+            echo "</tr><tr bgcolor=".COLOR_IMPAR.">";
+        }else{
+            echo "</tr><tr bgcolor=".COLOR_PAR.">";
+        }
+        $fila++;
     }
 }
     
